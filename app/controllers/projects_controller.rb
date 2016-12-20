dc9fa5ba5d48509b5e0cd7f11bc15fe6fd74d625
Kweku boateng <fredshavnqweku@gmail.com>
2016-12-20 10:51:45 +0000

    'create a new project' feature complete.

---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,38 @@
+class ProjectsController < ApplicationController
+   def index
+   end
+
+   def new
+     @project = Project.new
+   end
+
+   def create
+     @project = Project.new(project_params)
+
+     if @project.save
+       flash[:notice] = "Project has been created."
+       redirect_to @project
+     else
+     end
+   end
+
+   def show
+     @project = Project.find(params[:id])
+   end
+
+   private
+
+   def project_params
+     params.require(:project).permit(:name, :description)
+   end
+
+   def update
+   end
+
+   def destroy
+   end
+
+   def edit
+   end
+
+end
